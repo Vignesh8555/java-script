32f523ea2f7f8b62e210de59ced973461c143708
vignesh M <vigneshmurugaraj8555@gmail.com>
2025-07-11 10:38:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var x=3;
+let name=`js`;
+const iscloudy=true;
+console.log(x+ ""+name+""+iscloudy);
+
+function add(x,y){
+    console.log("infinity")
+}add(5,6);
+let b=parseInt(prompt("enter the value"));
+
+alert(a);
